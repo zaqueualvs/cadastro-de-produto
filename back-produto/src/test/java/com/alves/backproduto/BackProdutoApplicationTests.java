988efce5640ed73212bc5f03446d5c4381ec08ee
zaqueu alves <zaqueu.za15@gmail.com>
2024-01-25 16:52:50 -0300

    inicio

---
+++ back-produto/src/test/java/com/alves/backproduto/BackProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.backproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
